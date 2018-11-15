131c73660a4013492da24a8e48141b9cfc32d91f
Areliez Vargas <areliez.vargas@gmail.com>
2018-11-15 12:50:19 -0400

    Open photo with differents web browsers

--- src/main/java/FirstExample.java
+++ src/main/java/FirstExample.java
@@ -1,31 +1,79 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class FirstExample {
 
     public static void main(String[] args) throws InterruptedException {
 
-        System.out.println("Hi");
-        System.setProperty("webdriver.gecko.driver", "C:\\\\dev\\tools\\geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
-        driver.get("http://www.google.com");
+        WebDriver driverFirefox = getDriver(0);
+        openImage(driverFirefox);
+        closeDriver(driverFirefox);
 
-        driver.findElement(By.id("lst-fb")).click();
-        driver.findElement(By.id("lst-fb")).sendKeys("Naruto");
-        driver.findElement(By.name("btnR")).click();
+        WebDriver driverChrome = getDriver(1);
+        openImage(driverChrome);
+        closeDriver(driverChrome);
 
-        driver.findElement(By.linkText("imagenes")).click();
-        List<WebElement> imagenes = driver.findElements(By.tagName("img"));
+        WebDriver driverEdge = getDriver(2);
+        openImage(driverEdge);
+        closeDriver(driverEdge);
 
-        if(imagenes.size() > 2) {
-            imagenes.get(2).click();
-        }
-
-        Thread.sleep(25000);
-        driver.quit();
+        WebDriver driverOpera = getDriver(3);
+        openImage(driverOpera);
+        closeDriver(driverOpera);
     }
+
+    private static void openImage(WebDriver driver) {
+        driver.get("http://www.google.com/");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(6, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("lst-ib")).click();
+        driver.findElement(By.id("lst-ib")).sendKeys("Naruto");
+
+        driver.findElement(By.name("btnK")).click();
+
+        driver.findElement(By.linkText("Imágenes")).click();
+        List<WebElement> imagenes = driver.findElements(By.tagName("img"));
+        imagenes.get(2).click();
+
+    }
+
+    private static void closeDriver(WebDriver driver1) throws InterruptedException {
+        Thread.sleep(10000);
+        driver1.quit();
+    }
+
+    //browsername should be an enum
+    private static WebDriver getDriver(int browserName) {
+        WebDriver driver1 = null;
+        switch (browserName) {
+            case 0:
+                System.setProperty("webdriver.gecko.driver", "C:\\dev\\tools\\firefoxTools\\geckodriver.exe");
+                driver1 = new FirefoxDriver();
+                break;
+            case 1:
+                System.setProperty("webdriver.chrome.driver", "C:\\dev\\tools\\chromeTools\\chromedriver.exe");
+                driver1 = new ChromeDriver();
+                break;
+            case  2:
+                System.setProperty("webdriver.edge.driver", "C:\\dev\\tools\\edgeTools\\MicrosoftWebDriver.exe");
+                driver1 = new EdgeDriver();
+                break;
+            case 3:
+                System.setProperty("webdriver.opera.driver", "C:\\dev\\tools\\operaTools\\operadriver.exe");
+                driver1 = new OperaDriver();
+                break;
+        }
+        return driver1;
+    }
+    
 }
